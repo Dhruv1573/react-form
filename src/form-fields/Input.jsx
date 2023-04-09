d4ffb3b7cf05b3409faf9e006418aa51cf16998c
Dhruv kumar <36184412+Dhruv1573@users.noreply.github.com>
2023-04-09 11:12:12 +0530

    new commit

--- src/form-fields/Input.jsx
+++ src/form-fields/Input.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import "./Style.css"
 const InputField = ({label,onChange,...otherProps}) => {
   return (
-        
-        <div className='inputField'>
-        <label>{label}</label>
-        <input {...otherProps} onChange={onChange}/>
-        <span>Error Mesage</span>
-        </div>
-
-  )
+      <div className='inputField'>
+      <label>{label}</label>
+      <input {...otherProps} onChange={onChange} />
+      <span>Error Mesage</span>
+      </div>
+  );
 }
 
 export default InputField;
